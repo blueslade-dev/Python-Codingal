48e0406e0f90a737d4d5be2a2e59b36a2c87b691
blueslade-dev <blueslade05@gmail.com>
2026-07-25 16:44:31 +0530

    Add files via upload

---
+++ Dailyroutine.py
@@ -0,0 +1,36 @@
+# My daily routine
+
+# Wake up time
+print("Wake up time: 6.30 am")
+
+# Bus boarding time
+print("Boarding bus at: 7.15 am")
+
+# Reach school time
+print("School reaching time: 8.00 am")
+
+# Breakfast and Lunch Time
+print("Breakfast Time: 8.30 am")
+print("Lunch time: 12.30 pm")
+
+# School end/ dispersal time 
+print("School ends at 3:30 pm")
+
+# Return Home time
+print("Returning home by 4:30 pm")
+
+# Screen Time
+print("Screen Time: 5 pm to 6pm")
+
+# Study Time
+print("Study Time: 6 pm to 10:30pm")
+
+# Dinner Time
+print("Dinner Time: 10:30 pm")
+
+#Sleep Time
+print("11 pm to 6:30 am")
+
+# Weekly study time = daily study time * 7
+# Daily study time = 4.5 hours
+print("Weekly study time:", 4.5 * 7, "hours")
