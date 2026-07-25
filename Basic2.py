8154b7b27b98b8dbc96e03a9cff8489c86bf1951
blueslade-dev <blueslade05@gmail.com>
2026-07-25 12:36:41 +0530

    Add files via upload

---
+++ Basic2.py
@@ -0,0 +1,31 @@
+# My Introduction
+
+# printing my name
+print("I'm Krithik")
+
+# printing my birthplace
+print("I'm born in Kolkata")
+
+#printing my hobby
+print("My hobby is playing video games")
+
+#printing my favourite subject
+print("My favourite subject is Computer Science")
+
+# printing my dream
+print("I want to be a programmer")
+
+# printing my game name
+print("I play Roblox")
+
+#printing my age
+print("My age is 13")
+
+# printing my birthday
+print("My birthday on 1st July 2013")
+
+# printing my dream college
+print("I want to get into IIT Bombay")
+
+#printing my best friend name
+print("My best friend is Saksham")
