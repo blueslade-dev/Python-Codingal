db86d5e810e63f668078536bcb252fe849929732
blueslade-dev <blueslade05@gmail.com>
2026-07-25 12:26:23 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,11 @@
+# Printing hello world
+print("Hello World!")
+
+#Printing my name
+print("I'm Krithik")
+
+#Printing my hobby
+print("I like to do gaming")
+
+# Performance in test and simple calculation.
+print("My score is:",15+12)
