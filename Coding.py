58bce260b39f732abb84d36ec989b3c3732a89d2
blueslade-dev <blueslade05@gmail.com>
2026-07-26 12:29:55 +0530

    Add files via upload

---
+++ Coding.py
@@ -0,0 +1,14 @@
+# Print a string
+print("Welcome to the world of programming")
+
+#printing a number
+print(6)
+
+# inserting new line
+print("I like to do coding \n")
+
+# multiple argument
+print("I am ",13)
+
+#end argument
+print("Print",end="6") 
---
+++ Coding2.py
@@ -0,0 +1,11 @@
+# Making variables
+x = 81
+y = "Hello"
+#Printing them
+print(x)
+print(y)
+#Making another variable and printing it
+a = "codingal"
+print(a)
+
+name = input("Enter your name:")
