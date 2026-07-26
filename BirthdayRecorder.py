7df4df57dd6523bcc8dd7f28c1bcaa7bdc515d28
blueslade-dev <blueslade05@gmail.com>
2026-07-26 16:59:10 +0530

    Add files via upload

---
+++ BirthdayRecorder.py
@@ -0,0 +1,10 @@
+my_birthday = "1st July"
+a = "10th April"
+b = "18th September"
+c= "12th April"
+d = "26th October"
+print("My birthday is on: ", my_birthday)
+print("My father's birthday on: ",a)
+print("My mother's birthday on: ",b)
+print("My grandma's birthday on: ",c)
+print("My best friend's birthday on:", d, end = "|")
