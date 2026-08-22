8e08402696706556c0993e745952a0775e579f54
blueslade-dev <blueslade05@gmail.com>
2026-08-22 12:30:02 +0530

    Add files via upload

---
+++ identityoperator.py
@@ -0,0 +1,13 @@
+x = 5
+if type(x) is int:
+    print("true")
+else:
+    print("false")
+
+x = 20
+y = 20
+if x is y:
+    print("x and y are same identity")
+else:
+    print("x and y are not same identity")
+
