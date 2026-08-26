68acaf3f235ff16380ed15c6b555183b69dc7d49
blueslade-dev <blueslade05@gmail.com>
2026-08-26 12:26:35 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,11 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+
+total_numbers =40
+sum = total_numbers * mean1
+num2 = sum - (wrong_number-correct_number)
+print("Num2 is(sum - (wrong_number-correct_number)): ", num2)
+
+mean2 = num2/ total_numbers
+print(mean2)
