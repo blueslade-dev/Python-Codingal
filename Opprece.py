1b4770d30b87889d0a231e860fca894994191bc9
blueslade-dev <blueslade05@gmail.com>
2026-08-26 12:08:21 +0530

    Add files via upload

---
+++ Opprece.py
@@ -0,0 +1,13 @@
+x = 5
+y = 7
+z = 3
+w = 6
+v = (x-y) * z / w
+print("Value of (x-y) * z / w is", v)
+
+name = "Rohit"
+age = 5
+if name == "Rohit" or name == "Rammohan" and age >= 4:
+    print("You can play with this toy")
+else:
+    print("You cannot play with this toy")
